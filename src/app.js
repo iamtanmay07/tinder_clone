dcaea259ba863e5f13f740b161abd45e70e4f5a4
Tanmay <tanmaypatel26704@gmail.com>
2025-03-27 17:48:34 +0530

    added middlewares

--- src/app.js
+++ src/app.js
@@ -2,10 +2,37 @@ const express = require('express');
 
 const app = express();
 
-app.use((req,res) => {
-    res.send('Hello World!!');
-})
+app.use(
+    "/admin/getAllData", (req, res, next) => {
+        const token = "xz";
+        const isAuth = token === "xyz";
+        if(isAuth){
+            res.send("got all the data!");
+        }
+        else{
+            res.status(401).send("Not Authenticated!!");
+        }
+    },
+);
+
+app.use(
+    "/admin/deleteUser", (req, res, next) => {
+        const token = "xyz";
+        const isAuth = token === "xyz";
+        if(isAuth){
+            res.send("deleted the user");
+        }
+        else{
+            res.status(401).send("Not Authenticated!!");
+        }
+    },
+);
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
 });
+
+/* output : 
+
+*/
