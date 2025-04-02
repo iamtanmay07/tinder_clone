8b9a05a423f10d25b09b38bd7961ee56045a1ab3
Tanmay <tanmaypatel26704@gmail.com>
2025-04-02 17:04:05 +0530

    schema validation

--- src/app.js
+++ src/app.js
@@ -4,15 +4,20 @@ require("./config/database");
 const app = express();
 const User = require("./models/user");
 
+// needed JSON parsing middleware
+app.use(express.json());
 
+// signup the user using POST API
 app.post("/signup", async (req, res) => {
     
-    const userObj ={
-        firstName: req.body.firstName || "Tanmay",
-        lastName: req.body.lastName || "Patel",
-        emailId: req.body.emailId || "tanmay9248@gmail.com",
-        password: req.body.password || "password",
-    }
+    // const userObj ={
+    //     firstName: req.body.firstName || "Tanmay",
+    //     lastName: req.body.lastName || "Patel",
+    //     emailId: req.body.emailId || "tanmay9248@gmail.com",
+    //     password: req.body.password || "password",
+    // }
+
+    const userObj = req.body; // destructure the data from the request body
 
     // creating instance of User model 
     // always use try-catch block to handle errors
@@ -22,19 +27,75 @@ app.post("/signup", async (req, res) => {
         res.send("User signed up");
         console.log("User signed up log");
     } catch(err){
-        // console.error(err);
+        console.error(err);
         res.status(500).send("Error signing up user");
     }
 }); 
 
-// GET API to fetch all users
-app.get("/users", async (req, res) => {
+// GET API to find the user by email
+app.get("/user", async (req, res) => {
+    const userEmail = req.body.emailId;
     try {
-        const users = await User.find(); // Fetch all users from the database
-        res.status(200).json(users); // Send the users as a JSON response
+        // Fetch all users from the database
+        const user = await User.find({emailId : userEmail}); 
+        res.status(200).send(user); // Send the users as a JSON response
         console.log("Fetched users log");
     } catch (error) {
         res.status(500).send("Error fetching users");
+        console.error(error);
+    }
+});
+
+// GET API to find all the users
+app.get("/feed", async (req, res) => {
+    const userEmail = req.body.emailId;
+    try {
+        // Fetch all users from the database
+        const user = await User.find(); 
+        res.status(200).send(user); // Send the users as a JSON response
+        console.log("Fetched users log");
+    } catch (error) {
+        res.status(500).send("Error fetching users");
+        console.error(error);
+    }
+});
+
+// DELETE API to delete the user
+app.delete("/user", async (req, res) => {
+    const userId = req.body.userId;
+    try {
+        // const user = await User.findOneAndDelete({_id: userId});
+        const user  = await User.findByIdAndDelete(userId);
+
+        if (!user) {
+            // If no user is found, return 404
+            return res.status(404).send("User not found");
+        }
+        res.send("user is deleted"); 
+        console.log("Deletion completed");
+    } catch (error) {
+        res.status(500).send("Error deleting user!");
+        console.error(error);
+    }
+});
+
+// User update API 
+app.patch("/user", async (req, res) => {
+    const userId = req.body.userId;
+    const data = req.body;
+
+    try {
+        const user  = await User.findByIdAndUpdate(userId, data, { new: true, runValidators: true });
+        
+        if (!user) {
+            // If no user is found, return 404
+            return res.status(404).send("User not found");
+        }
+
+        res.send(user); 
+        console.log("updation completed");
+    } catch (error) {
+        res.status(500).send("Error updating the user!");
         console.error(error);
     }
 });
--- src/models/user.js
+++ src/models/user.js
@@ -11,7 +11,10 @@ const userSchema = new mongoose.Schema({
     emailId:{
         type: String,
         required: true,
-        unique: true
+        unique: true, 
+        lowercase: true,
+        // trim white space 
+        trim: true,
     },
     password: {
         type: String,
@@ -19,10 +22,30 @@ const userSchema = new mongoose.Schema({
     },
     age: {
         type: Number,
+        min : 18,
     },
     gender :{
         type: String,
+        validate(value){
+            if(!["male", "female", "other"].includes(value)){
+                throw new Error("Gender data is not valid");
+            }
+        }
+    },
+    photoUrl :{
+        type : String,
+        default : "https://www.w3schools.com/howto/img_avatar.png",
+    },
+    skills: {
+        type: [String],
+    }, 
+    about :{
+        type : String,
+        default : "This is deafault about the user!",
+        MaxLength : 100,
     }
+}, {
+    timestamps: true,
 })
 
 module.exports = mongoose.model('User', userSchema);
